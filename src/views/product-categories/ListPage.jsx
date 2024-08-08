598b7069abc145fb219893ab236ccd6cc7f9ca12
bloodykheeng <69955065+bloodykheeng@users.noreply.github.com>
2024-08-08 13:14:48 +0300

    addded cloudinary file media manager

--- src/views/product-categories/ListPage.jsx
+++ src/views/product-categories/ListPage.jsx
@@ -127,11 +127,19 @@ function ListPage({ ...props }) {
             field: "details",
         },
 
+        // {
+        //     title: "Photo",
+        //     field: "photo_url",
+        //     render: (rowData) => {
+        //         return rowData.photo_url ? <Image src={`${process.env.REACT_APP_IMAGE_BASE_URL}${rowData.photo_url}`} alt={rowData.name} width="100" preview style={{ verticalAlign: "middle" }} /> : <div>No Image</div>;
+        //     },
+        // },
+
         {
             title: "Photo",
-            field: "photo_url",
+            field: "cloudinary_photo_url",
             render: (rowData) => {
-                return rowData.photo_url ? <Image src={`${process.env.REACT_APP_IMAGE_BASE_URL}${rowData.photo_url}`} alt={rowData.name} width="100" preview style={{ verticalAlign: "middle" }} /> : <div>No Image</div>;
+                return rowData.cloudinary_photo_url ? <Image src={`${rowData.cloudinary_photo_url}`} alt={rowData.name} width="100" preview style={{ verticalAlign: "middle" }} /> : <div>No Image</div>;
             },
         },
 
