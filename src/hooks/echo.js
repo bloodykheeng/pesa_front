6b17055f572722ef934f02b00f18b11d5041f236
bloodykheeng <69955065+bloodykheeng@users.noreply.github.com>
2024-08-23 07:01:30 +0300

    added laravel echo and pusher js

---
+++ src/hooks/echo.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import axios from "axios"; // Import axios directly from the library
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+const useEcho = () => {
+  const [echoInstance, setEchoInstance] = useState(null);
+
+  useEffect(() => {
+    // Create the Echo instance here...
+    const echo = new Echo({
+      broadcaster: "pusher", // Assuming you're using Pusher
+      key: process.env.REACT_APP_REVERB_APP_KEY,
+      authorizer: (channel) => {
+        return {
+          authorize: (socketId, callback) => {
+            axios
+              .post("/api/broadcasting/auth", {
+                socket_id: socketId,
+                channel_name: channel.name
+              })
+              .then((response) => {
+                callback(false, response.data);
+              })
+              .catch((error) => {
+                callback(true, error);
+              });
+          }
+        };
+      },
+      wsHost: process.env.REACT_APP_REVERB_HOST,
+      wsPort: process.env.REACT_APP_REVERB_PORT ?? 80,
+      wssPort: process.env.REACT_APP_REVERB_PORT ?? 443,
+      forceTLS: (process.env.REACT_APP_REVERB_SCHEME ?? "https") === "https",
+      enabledTransports: ["ws", "wss"]
+    });
+
+    setEchoInstance(echo);
+
+    // Cleanup function to disconnect Echo instance when component unmounts
+    return () => {
+      echo.disconnect();
+    };
+  }, []);
+
+  return echoInstance;
+};
+
+export default useEcho;
